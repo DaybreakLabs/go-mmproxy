52697e4a3f58a3b15f05aaa0ca5869920ea08f31
dreamsvxyz <dream@dreamsv.xyz>
2025-01-09 02:37:57 +0900

    feat: cloudflare spectrum

--- udp/udp.go
+++ udp/udp.go
@@ -164,10 +164,19 @@ func Listen(ctx context.Context, listenConfig *net.ListenConfig, opts *utils.Opt
 			continue
 		}
 
-		saddr, daddr, restBytes, err := proxyprotocol.ReadRemoteAddr(buffer[:n], utils.UDP)
-		if err != nil {
-			logger.Debug("failed to parse PROXY header", "error", err, slog.String("remoteAddr", remoteAddr.String()))
-			continue
+		var saddr, daddr netip.AddrPort
+		var restBytes []byte
+
+		if conn, exists := connectionMap[remoteAddr]; !exists {
+			saddr, daddr, restBytes, err = proxyprotocol.ReadRemoteAddr(buffer[:n], utils.UDP)
+			if err != nil {
+				logger.Debug("failed to parse PROXY header", "error", err, slog.String("remoteAddr", remoteAddr.String()))
+				continue
+			}
+		} else {
+			restBytes = buffer[:n]
+			saddr = conn.clientAddr
+			daddr = conn.downstreamAddr
 		}
 
 		for {
